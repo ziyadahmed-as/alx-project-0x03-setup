719169aeebb80c8d8b3307cb8f2a0f95e69ccaf6
Ziyad Ahmed <ziyafwru.12@gmail.com>
2025-09-28 17:35:14 +0300

    Implement create placeholder pages for /generate-text-ai, /text-to-image, and /counter-app

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,9 +1,47 @@
-const Home = () => {
-  return (
-    <div>
-      <h1> Welcome to Splash App </h1>
-    </div>
-  )
+import Button from "@/components/common/Button";
+import { useRouter } from "next/router";
+
+interface PageRouteProps {
+  pageRoute: string
 }
 
-export default Home;
+export default function Home() {
+  const router = useRouter()
+
+  // Imperative routing with useRouter
+  const routeToNextPage  = ({ pageRoute }: PageRouteProps) => {
+    router.push(pageRoute, undefined, { shallow: false })
+  }
+
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center text-center">
+      {/* Welcome Message */}
+      <h1 className="text-4xl font-bold text-gray-800 mb-4">
+        Welcome to Splash App!
+      </h1>
+      <p className="text-lg text-gray-600 mb-8">
+        Your one-stop platform for everything AI you need. Start exploring by
+        navigating to our features below.
+      </p>
+
+      {/* Navigation Options */}
+      <div className="flex gap-6">
+        <Button 
+          action={() => routeToNextPage({ pageRoute: '/generate-text-ai' })} 
+          buttonLabel="Generate Text" 
+          buttonBackgroundColor="blue" 
+        />
+        <Button 
+          action={() => routeToNextPage({ pageRoute: '/text-to-image'})} 
+          buttonLabel="Text to Image" 
+          buttonBackgroundColor="green" 
+        />
+        <Button 
+          action={() => routeToNextPage({ pageRoute: '/counter-app'})} 
+          buttonLabel="Contact us" 
+          buttonBackgroundColor="orange" 
+        />
+      </div>
+    </div>
+  );
+}
